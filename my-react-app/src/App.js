4f2cf1c6e866e584351fe6898886cf418d7c91aa
gutMB <gut.pudinan@gmail.com>
2021-04-04 21:27:11 +0700

    setting format

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,9 +1,5 @@
-import React from 'react'
+import React from "react";
 
-export default function App() {
-  return (
-    <div>
-      abc
-    </div>
-  )
-}
+export default () => {
+  return <div>abc</div>;
+};
