0a638296e58b3c3d3bfa8dba5b3aad6220f8ca38
gutMB <gut.pudinan@gmail.com>
2021-05-05 01:04:11 +0700

    -wait for fetch

--- my-react-app/src/MapPage/MapPage.js
+++ my-react-app/src/MapPage/MapPage.js
@@ -1,23 +1,21 @@
 import React, { useState, useContext, useEffect } from "react";
-import MapContext from "../components/MapContext";
+import MapContext from "./components/MapContext";
 
-import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
+import {
+  GoogleMap,
+  useLoadScript,
+  Marker,
+  InfoWindow,
+} from "@react-google-maps/api";
 
 import mapStyle from "./Style/mapStyle.js";
 import "./Style/map.css";
 
-import { useLocation } from "react-router-dom";
+// import { useLocation } from "react-router-dom";
 
 //------------map style--------------------
 const libraries = ["places"];
-const mapContainerStyle_Home = {
-  width: "91.6vw",
-  height: "52vh",
-  maxWidth: "1024px",
-  borderRadius: "0.5rem 0.5rem 0rem 0rem",
-  boxShadow: "5px 10px 10px rgba(0,0,0,0.1)",
-};
-const mapContainerStyle_PillStore = {
+const mapContainerStyle = {
   width: "91.6vw",
   height: "70vh",
   maxWidth: "1024px",
@@ -27,21 +25,47 @@ const mapContainerStyle_PillStore = {
 const options = {
   styles: mapStyle,
 };
-const center = {
+const defaultCenter = {
   lat: 14.19947,
   lng: 101.209671,
 };
+const demo = [
+  {
+    id: "12313",
+    avatarUri: "https://api.pillplus.store/api/v1/picture/avatar.jpg",
+    openingStatus: true,
+    activated: true,
+    status: true,
+    name: "โรงพยาบาลลาดกระบัง",
+    pharmacy: "โรงพยาบาล ลาดกระบัง",
+    location: "2 Lat Krabang 15 Alley, Lat Krabang, Bangkok 10520",
+    phone: "023269995",
+    email: "ladkrabang@hospital.com",
+    password: "$2b$10$/GCA3FMVPimaqqRt4CwcROgb7awTD.F0s9tm88Cm3u4ziXUYc0y42",
+    ID: "PS1000",
+    openingData: [],
+    coordinate: { lat: 13.56321, lng: 100.616373 },
+  },
+];
 
 export default function MapPage() {
   //--------------map load-------------------
   const { isLoaded, loadError } = useLoadScript({
     googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
-    libraries,
+    libraries: libraries,
   });
+
+  //---------attibute------------
+  const [selected, setSelected] = useState(null);
+  const [pillStoreList, setPillStoreList] = useState([]);
+  const [center, setCenter] = useState(defaultCenter);
+
   const mapRef = React.useRef();
   const onMapLoad = React.useCallback((map) => {
     mapRef.current = map;
   }, []);
+
+  //---------fetch data----------
 
   //--------for function focus center---------
   const panTo = React.useCallback(({ lat, lng }) => {
@@ -49,38 +73,14 @@ export default function MapPage() {
     mapRef.current.setZoom(14);
   }, []);
 
-  //---------attibute------------
-  const [myLocation, setMyLocation] = useState(center);
-  const [markers, setMarkers] = useState(center);
   const onMapClick = React.useCallback((event) => {
-    setMarkers({
+    setCenter({
       lat: event.latLng.lat(),
       lng: event.latLng.lng(),
     });
-  });
-
-  //---------fetch data----------
-  useEffect(() => {
-    const fetchLocations = async () => {
-      const myL = await navigator.geolocation.getCurrentPosition(
-        (pos) => {
-          const latlng = {
-            lat: pos.coords.latitude,
-            lng: pos.coords.longitude,
-          };
-          setMyLocation(latlng);
-          setMarkers(latlng);
-        },
-        () => null
-      );
-    };
-
-    fetchLocations();
+    setSelected(null);
+    setPillStoreList(demo);
   }, []);
-
-  //----------check path------------
-  const location = useLocation();
-  const isHomePath = location.pathname === "/home";
 
   //-------check loading-----------
   if (loadError) return "Error loading maps";
@@ -90,36 +90,70 @@ export default function MapPage() {
     <div>
       <GoogleMap
         id="map"
-        mapContainerStyle={
-          isHomePath ? mapContainerStyle_Home : mapContainerStyle_PillStore
-        }
+        mapContainerStyle={mapContainerStyle}
         zoom={14}
-        center={myLocation}
+        center={center}
         options={options}
         onLoad={onMapLoad}
         onClick={onMapClick}
       >
-        <MapContext.Provider value={{ setMyLocation, setMarkers, myLocation }}>
+        <MapContext.Provider value={{ setCenter }}>
           <Locate panTo={panTo} />
         </MapContext.Provider>
 
-        <Marker
-          key="123"
-          position={markers}
-          icon={{
-            url: "https://i.imgur.com/Ei2X1hR.png", //green
-            scaledSize: new window.google.maps.Size(40, 40),
-            origin: new window.google.maps.Point(0, 0),
-            anchor: new window.google.maps.Point(15, 15),
-          }}
-        />
+        {pillStoreList.map(
+          (pillStore) =>
+            true &&
+            (pillStore.status ? (
+              <Marker
+                key={pillStore.id}
+                position={pillStore.coordinate}
+                icon={{
+                  url: "https://i.imgur.com/Ei2X1hR.png", // green
+                  scaledSize: new window.google.maps.Size(40, 40),
+                  origin: new window.google.maps.Point(0, 0),
+                  anchor: new window.google.maps.Point(15, 15),
+                }}
+                onClick={() => {
+                  setSelected(pillStore);
+                }}
+              />
+            ) : (
+              <Marker
+                key={pillStore.id}
+                position={pillStore.coordinate}
+                icon={{
+                  url: "https://i.imgur.com/v4dw84y.png", //red
+                  scaledSize: new window.google.maps.Size(40, 40),
+                  origin: new window.google.maps.Point(0, 0),
+                  anchor: new window.google.maps.Point(15, 15),
+                }}
+                onClick={() => {
+                  setSelected(pillStore);
+                }}
+              />
+            ))
+        )}
+        {selected ? (
+          <InfoWindow
+            position={selected.coordinate}
+            onCloseClick={() => {
+              setSelected(null);
+            }}
+          >
+            <div>
+              <h2>{selected.pharmacy}</h2>
+              <p>{selected.location}</p>
+            </div>
+          </InfoWindow>
+        ) : null}
       </GoogleMap>
     </div>
   );
 }
 
 function Locate({ panTo }) {
-  const { setMyLocation, setMarkers } = useContext(MapContext);
+  const { setCenter } = useContext(MapContext);
   return (
     <div>
       <button
@@ -131,8 +165,7 @@ function Locate({ panTo }) {
                 lat: position.coords.latitude,
                 lng: position.coords.longitude,
               };
-              setMyLocation(latlng);
-              setMarkers(latlng);
+              setCenter(latlng);
               panTo({
                 lat: latlng.lat,
                 lng: latlng.lng,
